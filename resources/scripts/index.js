c43ecb32ed874715b8d4291dcdc79e29ab6bca4d
Ruikang Lin <ruikanglin@outlook.com>
2024-10-14 22:19:57 -0400

    Portfolio Section Template + Bug Fixes

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -9,6 +9,7 @@
 const tab_btns = document.querySelectorAll(".tab_btn");
 const tab_panels = document.querySelectorAll(".tab_panel");
 const tab_indicator = document.getElementById("tab_indicator");
+let active_tab;
 
 tab_indicator.style.width = tab_btns[0].getBoundingClientRect().width + "px";
 tab_indicator.style.left = tab_btns[0].getBoundingClientRect().left - tab_btns[0].parentElement.getBoundingClientRect().left + "px";
@@ -25,5 +26,16 @@ tab_btns.forEach(tab_btn => {
                 tab_panel.classList.add("hidden")
             }
         });
+        active_tab = tab_btn;
+        console.log(active_tab);
     });
 });
+
+// fix the indictor size on orientation change on mobile devices
+window.addEventListener("orientationchange", function() {
+    setTimeout(function(){
+        tab_indicator.style.width = active_tab.getBoundingClientRect().width + "px";
+        tab_indicator.style.left = active_tab.getBoundingClientRect().left - active_tab.parentElement.getBoundingClientRect().left + "px";
+    }, 5);
+	
+}, false);
